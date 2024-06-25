90e99076a0ebb3333bf803f4dcf96bb5dba0b7cc
Elizabeth <itsalaz@icloud.com>
2024-06-25 16:41:10 -0400

    lab completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad () {
+    function change() {
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <div>
+            <input 
+            type = "password" 
+            onChange= {change}
+            />
+        </div>
     )
 }
 
